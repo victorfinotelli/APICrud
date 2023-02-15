bd1b1fd08aad96b04bac346fcc629537aa62dc3c
Victor <finotellipro@gmail.com>
2023-02-14 22:34:10 -0300

    commit 1.2

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
+
 
 app.MapPost("/products", (Product product) => {
     ProductRepository.Add(product);
@@ -12,6 +15,7 @@ app.MapGet("/products/{code}", ([FromRoute] string code) =>{
     var product = ProductRepository.GetBy(code);
     if(product != null)
     {
+        Console.WriteLine("Product found");
         return Results.Ok(product);
     }
         return Results.NotFound();
@@ -29,16 +33,23 @@ app.MapDelete("/products/{code}", ([FromRoute] string code) => {
    return Results.Ok();
 });
 
+//Esse IF filtra o ambiente, ou seja só roda se estiver como Staging na Propertios/launchSettings.json (Por padrão mantive Staging)
+if(app.Environment.IsStaging()){
+    app.MapGet("/configuration/database", (IConfiguration configuration) => {
+        return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+    });
+}
 app.Run();
 
 public static class ProductRepository{
-    public static List<Product> Products {get; set;}
+    public static List<Product> Products {get; set;} = Products = new List<Product>();
 
+    public static void Init(IConfiguration configuration){
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
     public static void Add(Product product){
-        if(Products == null)
-        Products = new List<Product>();
-
-        Products.Add(product);
+           Products.Add(product);
     }
 
     public static Product GetBy(string code){
--- README.txt
+++ README.txt
@@ -1,14 +1,12 @@
 No postman para testar o Post basta colocar a seguinte URL com seus respectivos end points:
 
-Post: https://localhost:3001/saveproduct
+Post: https://localhost:3001/products
 Jason no body: exemplo {"code": "3", "name": "teste"}
 
-Get: https://localhost:3001/getproduct/3
-Ex: o code 3 é uma referencia para select ao post acima.
+Get: https://localhost:3001/products/3 (Ex: o code 3 é uma referencia).
 
-Put: https://localhost:3001/editproduct
+Put: https://localhost:3001/products
 Json no body: {"code": "3", "name": "Haha++++"}
 Ele referencia ao code do primeiro exemplo e altera o seu name.(O método só permite alteração no name por conta do code ser um Id).
 
-Del: https://localhost:3001/deleteproduct/3
-Ex: Referencia ao code 3 que é referencia do post.
+Del: https://localhost:3001/products/3 (Ex: Referencia ao code 3).
